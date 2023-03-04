510c1c2688c4d0f37974ee549a545b537101466f
KevinHernandezH2 <kevin.hernandez8@catolica.edu.sv>
2023-03-04 03:55:38 -0600

    proyecto

---
+++ webApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webApiPractica.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting.Server;
+using System.Diagnostics.Metrics;
+using static webApiPractica.Models.equipo;
+
+namespace webApiPractica.Properties
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+
+        }
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
+
+            if (listadoEquipo.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoEquipo);
+
+        }
+    }
+
+}
